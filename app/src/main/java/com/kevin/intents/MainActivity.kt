0c57f47b7fb79a45cc035e8f52ab8bd348225bda
A.v.s.Pavan <132935837+Kris777na@users.noreply.github.com>
2023-10-04 12:20:02 -0500

    Added intents to make a call, open camera, open gallery, set alarm. Added run time permissions for camera and call

--- app/src/main/java/com/kevin/intents/MainActivity.kt
+++ app/src/main/java/com/kevin/intents/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.kevin.intents
 
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.AlarmClock
+import android.provider.MediaStore
+import androidx.core.app.ActivityCompat
 import com.kevin.intents.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -18,13 +22,108 @@ class MainActivity : AppCompatActivity() {
 
     private fun initViews() {
         binding.nextScreenBtn.setOnClickListener {
-            val switcher = Intent(this, SecondActivity::class.java)
-            startActivity(switcher)
+            navigateNextScreen()
         }
 
         binding.webBrowserBtn.setOnClickListener {
-            val browser = Intent(Intent.ACTION_VIEW, Uri.parse("https://www.google.com"))
-            startActivity(browser)
+            openBrowser()
+        }
+
+        binding.callBtn.setOnClickListener {
+            makeCall()
+        }
+
+        binding.galleryBtn.setOnClickListener {
+            openGallery()
+        }
+
+        binding.alarmBtn.setOnClickListener {
+            setAlarm()
+        }
+
+        binding.cameraBtn.setOnClickListener {
+            openCamera()
+        }
+
+
+    }
+
+    private fun setAlarm() {
+        val intent = Intent(AlarmClock.ACTION_SHOW_ALARMS)
+        startActivity(intent)
+    }
+
+    private fun openGallery() {
+        val intent = Intent(Intent.ACTION_VIEW)
+        startActivity(intent)
+    }
+
+    private val CAMERA_PERMISSION_CODE = 10
+    private val CALL_PERMISSION_CODE = 11
+
+    private fun makeCall() {
+
+        val phone = "+13612532921"
+        val callIntent = Intent(Intent.ACTION_CALL)
+        callIntent.data = Uri.parse("tel:$phone")
+
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.CALL_PHONE
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            startActivity(callIntent)
+        } else {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(android.Manifest.permission.CALL_PHONE),
+                CALL_PERMISSION_CODE
+            )
+        }
+
+    }
+
+    private fun openCamera() {
+        val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.CAMERA
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            startActivity(cameraIntent)
+        } else {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(android.Manifest.permission.CAMERA),
+                CAMERA_PERMISSION_CODE
+            )
+        }
+    }
+
+    private fun openBrowser() {
+        val browser = Intent(Intent.ACTION_VIEW, Uri.parse("https://www.google.com"))
+        startActivity(browser)
+    }
+
+    private fun navigateNextScreen() {
+        val switcher = Intent(this, SecondActivity::class.java)
+        startActivity(switcher)
+    }
+
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if (requestCode == CALL_PERMISSION_CODE) {
+            makeCall()
+        }
+        if (requestCode == CAMERA_PERMISSION_CODE) {
+            openCamera()
         }
     }
 }
